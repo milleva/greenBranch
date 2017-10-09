f97ea13a291c672d5917408f4553115ec3f9821f
milleva <emiller.arkko@gmail.com>
2017-10-09 21:22:43 +0300

    varmistelu

--- greenBranch/src/models/checklist.ts
+++ greenBranch/src/models/checklist.ts
@@ -4,9 +4,8 @@ export class Checklist{
   database = firebase.database();
 
   id: string;
-  name: string; //needed?
+  name: string;
   ownerId: string;
-  titles: Map<number, string>;
   checkboxes: Map<string, boolean>;
   radioGroups: Map<string, number>;
   numberInputs: Map<string, number>;
@@ -20,21 +19,45 @@ export class Checklist{
     this.textInputs = new Map<string, string>();
   }
 
+  setId(id){
+    this.id = id;
+  }
+
+  addCheckbox(key, value){
+    this.checkboxes.set(key, value);
+  }
+
+  addTextInput(key, value){
+    this.textInputs.set(key, value);
+  }
+
   //4 functions for adding pairs to the maps. Use them when saving checklist
 
   save(){//save this checklist to db
     let userRef = this.database.ref().child('users/' + this.ownerId);
     let checklistRef = userRef.child('checkLists/' + this.id);
 
+    //checklistRef.set({'name': this.name});
+
     //for each key in a map, store key and value to that maps ref
     let checkboxKeys = Array.from(this.checkboxes.keys());
     for(let key of checkboxKeys){
-      checklistRef.child('checkboxes').set({
-        key: this.checkboxes.get(key)
-      });
+      let path = 'checkboxes/' + key;
+      checklistRef.child(path).set(
+        this.checkboxes.get(key)
+      );
     }
 
-    let radioGroupKeys = Array.from(this.radioGroups.keys());
+    let textInputKeys = Array.from(this.textInputs.keys());
+    for(let key of textInputKeys){
+      let path = 'textInputs/' + key;
+      alert(this.textInputs.get(key));
+      checklistRef.child(path).set(
+        this.textInputs.get(key)
+      );
+    }
+
+    /*let radioGroupKeys = Array.from(this.radioGroups.keys());
     for(let key of checkboxKeys){
       checklistRef.child('radioGroups').set({
         key: this.radioGroups.get(key)
@@ -46,14 +69,9 @@ export class Checklist{
       checklistRef.child('numberInputs').set({
         key: this.numberInputs.get(key)
       });
-    }
+    }*/
 
-    let textInputKeys = Array.from(this.textInputs.keys());
-    for(let key of checkboxKeys){
-      checklistRef.child('textInputs').set({
-        key: this.textInputs.get(key)
-      });
-    }
+
 
 
   }
--- greenBranch/src/pages/checklists/mediaBuying/mediaBuying.ts
+++ greenBranch/src/pages/checklists/mediaBuying/mediaBuying.ts
@@ -4,6 +4,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import {NavController} from 'ionic-angular'; /*NavParams import might be useless*/
 
 /*import checklistpage(s?) for specific checklists*/
+import firebase from 'firebase';
+import {Checklist} from '../../../models/checklist';
 
 @Component({
   selector: 'page-media-buying',
@@ -13,7 +15,78 @@ import {NavController} from 'ionic-angular'; /*NavParams import might be useless
 export class MediaBuyingChecklistPage {
   form: FormGroup;
 
+
+  checkboxes = {
+    iOS: false,
+    android: false,
+    windows: false,
+    fbPlatform: false,
+    other1: false,
+    appsFlyer: false,
+    fbMediaSDK: false,
+    chartboost: false,
+    keyArt: false,
+    videoFootage: false,
+    playableBuild: false,
+    other2: false,
+    uponConsumption: false,
+    phone: false,
+    tablet: false,
+    both: false,
+    preferredDevices: false,
+    social: false,
+    video: false,
+    display: false,
+    crossPromotion: false,
+    burst: false
+  };
+  radioGroups = {
+    campaignType: ''
+  };
+  numberInputs = {
+    dailyInstalls: 0,
+    totalInstalls: 0,
+    targetCPI: 0,
+    maxCPI: 0,
+    totalBudget: 0,
+    DailyBudget: 0
+
+  };
+  textInputs = {
+    other1: '',
+    other2: '',
+    other3: '',
+    campaignStart: '',
+    campaignEnd: '',
+    minimumOS: '',
+    preferredDevices: '',
+    t1: '',
+    t2: '',
+    t3: '',
+    gender: '',
+    age: '',
+    other4: ''
+
+  };
   constructor(public navCtrl: NavController){
+  }
+
+  save(){
+    var user = firebase.auth().currentUser;
+    if(!user){
+      alert('user not logged in');
+      return;
+    }
+    var dbChecklistInstance = new Checklist(user.uid);
+    dbChecklistInstance.setId('01_');
+    for(var checkbox in this.checkboxes){
+      dbChecklistInstance.addCheckbox(checkbox, this.checkboxes[checkbox]);
+    }
+    for(var textInput in this.textInputs){
+      dbChecklistInstance.addTextInput(textInput, this.textInputs[textInput]);
+    }
+    dbChecklistInstance.save();
+
   }
 
 
